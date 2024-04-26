beb50b3c0a71cdec37f8e9888446aa35698e1f1b
KygenHack <148578932+KygenHack@users.noreply.github.com>
2024-04-26 12:58:14 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,10 +16,10 @@ const NavBar = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white rounded-box w-52">
-          <li><a>HOME</a></li>
-          <li><a>ABOUT</a></li>
-          <li><a>ROADMAP</a></li>
-          <li><a>TOKENOMICS</a></li>
+        <li><a href="https://frogspepe.xyz">HOME</a></li>
+          <li><a href="https://frogspepe.xyz/about">ABOUT</a></li>
+          <li><a href="https://frogspepe.xyz/roadmap">ROADMAP</a></li>
+          <li><a href="https://frogspepe.xyz/tokenomics">TOKENOMICS</a></li>
         </ul>
       </div>
       <div className='flex'>
@@ -28,10 +28,10 @@ const NavBar = () => {
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1 text-white">
-      <li><a>HOME</a></li>
-          <li><a>ABOUT</a></li>
-          <li><a>ROADMAP</a></li>
-          <li><a>TOKENOMICS</a></li>
+      <li><a href="https://frogspepe.xyz">HOME</a></li>
+          <li><a href="https://frogspepe.xyz/about">ABOUT</a></li>
+          <li><a href="https://frogspepe.xyz/roadmap">ROADMAP</a></li>
+          <li><a href="https://frogspepe.xyz/tokenomics">TOKENOMICS</a></li>
       </ul>
     </div>
     <div className="navbar-end">
@@ -59,6 +59,17 @@ const NavBar = () => {
   return (
     <div className="navbar dashboardbg">
     <div className="navbar-start">
+    <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="svgsize" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </div>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white rounded-box w-52">
+          <li><a href="https://frogspepe.xyz">HOME</a></li>
+          <li><a href="https://frogspepe.xyz/about">ABOUT</a></li>
+          <li><a href="https://frogspepe.xyz/roadmap">ROADMAP</a></li>
+          <li><a href="https://frogspepe.xyz/tokenomics">TOKENOMICS</a></li>
+        </ul>
+      </div>
       <div className='flex'>
       <img src="https://frogspepe.xyz/images/frogs.png" alt="" width={'84px'}></img>
       </div>
@@ -87,17 +98,6 @@ const NavBar = () => {
           "https://frogspepe.xyz"
         }
       />    
-      <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="svgsize" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white rounded-box w-52">
-          <li><a href="https://frogspepe.xyz">HOME</a></li>
-          <li><a href="https://frogspepe.xyz/about">ABOUT</a></li>
-          <li><a href="https://frogspepe.xyz/roadmap">ROADMAP</a></li>
-          <li><a href="https://frogspepe.xyz/tokenomics">TOKENOMICS</a></li>
-        </ul>
-      </div>
       </div>
   </div>
   )
