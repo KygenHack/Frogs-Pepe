df670bdfbe4d7212f9f6ded9b108d74082d4d8a4
KygenHack <148578932+KygenHack@users.noreply.github.com>
2024-04-26 13:12:27 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -59,12 +59,12 @@ const NavBar = () => {
   return (
     <div className="navbar dashboardbg">
     <div className="navbar-start">
-    <div className="dropdown">
+      <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="svgsize" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white rounded-box w-52">
-          <li><a href="https://frogspepe.xyz">HOME</a></li>
+        <li><a href="https://frogspepe.xyz">HOME</a></li>
           <li><a href="https://frogspepe.xyz/about">ABOUT</a></li>
           <li><a href="https://frogspepe.xyz/roadmap">ROADMAP</a></li>
           <li><a href="https://frogspepe.xyz/tokenomics">TOKENOMICS</a></li>
@@ -97,7 +97,7 @@ const NavBar = () => {
         privacyPolicyUrl={
           "https://frogspepe.xyz"
         }
-      />    
+      />
       </div>
   </div>
   )
